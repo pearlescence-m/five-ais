9b8122d669c079817c63b4a42cbdeaf3cce2c909
pearlescence-m <meruyert.jakipbekova@gmail.com>
2023-10-17 01:08:26 -0400

    firebase storage addition cleanup

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -1,5 +1,4 @@
 import { auth } from '@clerk/nextjs'
-import { writeFileSync } from 'fs'
 import { v4 as uuidv4 } from 'uuid'
 import { NextResponse } from 'next/server'
 import { HfInference } from '@huggingface/inference'
@@ -34,22 +33,15 @@ export async function POST(req: Request) {
         width: resolution,
       },
     })
-    console.log(response)
+
     const buf = await response.arrayBuffer()
-    console.log(buf)
 
     const fileName = `${uuidv4()}.jpeg`
-    // const filePath = `public/${fileName}`
-    // writeFileSync(filePath, new Uint8Array(buf), {mode: '0777'});
-    // console.log(`Done writing image to file ${fileName}`);
-
     const imageRef = ref(storage, `images/${fileName}`)
 
     await uploadBytes(imageRef, buf)
-    console.log('Uploaded a blob or file!')
-
     const url = await getDownloadURL(imageRef)
-    console.log("image link is ", url)
+
     return NextResponse.json(url)
     
   } catch (error) {
